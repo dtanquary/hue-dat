5cf752da2791f70ecfedc2288c7750b8eb17a983
Dave Tanquary <dtanquary@gmail.com>
2025-10-29 00:26:07 -0500

    Initial Commit

---
+++ hue dat Watch App/hue_datApp.swift
@@ -0,0 +1,17 @@
+//
+//  hue_datApp.swift
+//  hue dat Watch App
+//
+//  Created by David Tanquary on 10/29/25.
+//
+
+import SwiftUI
+
+@main
+struct hue_dat_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
